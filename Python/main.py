c9227bf2957affb0f8cc1e6127247b8b1cd49ef1
Imanol Gomez <yo@imanolgomez.net>
2019-11-06 11:44:50 +0100

    added Fonts and EDP functionality

--- Python/main.py
+++ Python/main.py
@@ -1,37 +1,53 @@
 import urllib.request
 import json 
+import epd7in5
 from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
 
-# Imported PIL Library from PIL import Image
+EPD_WIDTH = 640
+EPD_HEIGHT = 384
 
-url = 'https://api.nasa.gov/planetary/apod?api_key=n0OdkEMGNnqqBQ7RHSGTx7U4OnwrMCAporxkvCEu'
-response = urllib.request.urlopen(url)
-data = response.read()      # a `bytes` object
-text = data.decode('utf-8') # a `str`; this step can't be used if data is binary
+def main():
+    epd = epd7in5.EPD()
+    epd.init()
 
-# parse text:
-y = json.loads(text)
+    url = 'https://api.nasa.gov/planetary/apod?api_key=n0OdkEMGNnqqBQ7RHSGTx7U4OnwrMCAporxkvCEu'
+    response = urllib.request.urlopen(url)
+    data = response.read()      # a `bytes` object
+    text = data.decode('utf-8') # a `str`; this step can't be used if data is binary
 
-#the result is a Python dictionary:
-image_url  = y["url"]
-image_name = image_url.split('/')[-1]
-image_name = image_name.split('.')[0]
+    # parse text:
+    y = json.loads(text)
 
-image_path = "../Images/" + image_name + ".bmp"
+    #the result is a Python dictionary:
+    image_url  = y["url"]
+    image_name = image_url.split('/')[-1]
+    image_name = image_name.split('.')[0]
 
-print(image_url) 
-print(image_name)
-urllib.request.urlretrieve(image_url,image_path)
+    # PIL
+    font = ImageFont.truetype('../Fonts/roboto/Roboto-Bold.ttf', 24)
 
+    image_path = "../Images/" + image_name + ".bmp"
 
-print("Converting to dithering gray scale")
+    print(image_url) 
+    print(image_name)
+    urllib.request.urlretrieve(image_url,image_path)
 
-image = Image.open(image_path)
-newsize = (640, 384) 
-image = image.resize(newsize) 
-image = image.convert('1') 
+    print("Converting to dithering gray scale")
 
-image_path = "../Images/" + image_name + "_dithered.bmp"
+    image = Image.open(image_path)
+    newsize = (EPD_WIDTH, EPD_HEIGHT) 
+    image = image.resize(newsize) 
+    image = image.convert('1') 
 
-image.save(image_path)
+    image_path = "../Images/" + image_name + "_dithered.bmp"
+
+    image.save(image_path)
+
+    #EDP
+    epd.display_frame(epd.get_frame_buffer(image))
+
+if __name__ == '__main__':
+    main()
